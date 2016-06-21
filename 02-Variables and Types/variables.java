82def9741d935c4c2453ae1cdbc0907c86ff4945
Amir <amir_rajabifar@yahoo.com.au>
2016-06-21 23:54:23 +0800

    Variables and types

---
+++ 02-Variables and Types/variables.java
@@ -0,0 +1,40 @@
+class variables {
+ 	public static void main(String[] args) {
+ 		int touchdown; //a variable typ integer 
+ 		touchdown = 6; //store value 6 in "touchdown" variable
+ 		System.out.println (touchdown); //print the value of variable "touchdown"
+ 		
+ 		float varFloat = 1.2f; //to creat a float type variable the number must end in 'f'
+ 		double varDouble = 6.5;
+ 		char letter = 'L';
+ 		String sentence = "This is my string";
+ 		boolean stat = false;
+ 		System.out.println (varFloat);
+ 		System.out.println (varDouble);
+ 		System.out.println (letter);
+ 		System.out.println (sentence);
+ 		System.out.println (stat);
+ 	}
+} 
+/*
+Other types:
+
+Floating points:
+	float, 
+	double, 
+
+Words and letters:
+	char,
+	string,
+
+boolean
+takes two values true or false
+
+
+	- name the variable appropriately and relevantly.
+	- it's case sensitive
+	- if the name has multiple words it should be written as:
+		myVariableName
+	- can not use space, if space is required it should be written as:
+		my_variable_name
+*/

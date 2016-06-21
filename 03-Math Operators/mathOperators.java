6fc60470d1afc1f705702dbd2c3b97d907397165
Amir <amir_rajabifar@yahoo.com.au>
2016-06-22 00:00:59 +0800

    Math Operators

---
+++ 03-Math Operators/mathOperators.java
@@ -0,0 +1,16 @@
+class mathOperators {
+	public static void main(String[] args) {
+		int score = 40;
+		score = score + 10; // add 10 to the existing value of "score" variable
+
+		System.out.println (score);
+	}
+}
+
+/*
+Arithmatic operators:
+ + ==> addition	
+ - ==> subtract
+ / ==> devition
+ * ==> multiplication
+*/

96c0ee2629611fe62a528a4f47a792027c36926d
Amir <amir_rajabifar@yahoo.com.au>
2016-06-21 18:11:56 +0800

    First commit

---
+++ 2-Running a Java Program/helloJava.java
@@ -0,0 +1,13 @@
+class apples {
+	public static void main (String args[])
+	{
+		System.out.println("Hellow Java");
+	}
+}
+
+/* 
+To compile a java program:
+	javac <programname.java>
+To run a hava program:
+	java <classname.class>
+*/

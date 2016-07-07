685547059302334a96096618e9ca76e4c42416e2
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 01:32:33 +0800

    If and Else

---
+++ 06-If and Else Statments/ifStatment.java
@@ -0,0 +1,20 @@
+class ifStatment {
+	public static void main(String[] args){
+		int touchdown = 6;
+		if (touchdown == 6) {
+			System.out.print("A touchdown was scored\n");
+		}
+		else {
+			System.out.print("A touchdown was NOT scored\n");
+		}
+	}
+}
+
+/* 
+	== equal
+	!= not equal
+	>
+	<
+	<=
+	>=
+*/

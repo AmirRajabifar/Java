ea2353b7361a13022912ec120b553c29e0a46807
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 12:38:56 +0800

    Introduction to Objects

---
+++ 12-Objects/Planet.java
@@ -0,0 +1,7 @@
+public class Planet {
+	int size;
+	public void displaySize (){
+		System.out.print("Size of the planet is " + size);
+		System.out.print('\n');
+	}
+}
---
+++ 12-Objects/allPlanets.java
@@ -0,0 +1,7 @@
+public class allPlanets {
+	public static void main(String[] args) {
+		Planet Earth = new Planet ();
+		Earth.size = 25000;	
+		Earth.displaySize();
+	}
+}

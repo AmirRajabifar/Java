ff9cfd1f1c8a702452fd38da497961b4e039935d
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 12:19:51 +0800

    Arrays

---
+++ 11-Arrays/Arrays.java
@@ -0,0 +1,25 @@
+class Arrays 
+{
+	public static void main(String[] args) 
+	{
+		int[] numbers;
+		numbers = new int [5];
+		numbers[0] = 35;
+		numbers[1] = 45;
+		numbers[2] = 55;
+		numbers[3] = 65;
+		numbers[4] = 75;
+
+		System.out.print(numbers[1]);
+
+		String[] candy;
+		candy = new String [2];
+		candy[0] = "mint";
+		candy[1] = "rock";
+
+		System.out.print(candy[0]);
+
+		int[] num = {5, 9, 45};
+		System.out.print(num[1]);
+	}	
+}
